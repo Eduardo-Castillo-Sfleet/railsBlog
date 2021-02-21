73971254537581012c402364db4a8f0a83b0774d
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-02-21 16:13:02 -0600

    url mails

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,5 +1,9 @@
 class ArticleMailer < ApplicationMailer
     def new_artile(article)
-        
+        @article = article
+
+        User.all.each do |user|
+            mail(to: user.email, subject: "Nuevo artículo")
+        end
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  #For mailing
+  config.action_mailder.default_url_option = { host: "localhost:3000" }
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
